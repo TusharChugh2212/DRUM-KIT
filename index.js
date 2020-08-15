7368f595b69e94ba01fb04dde4e96f86566cf41d
TusharChugh2212 <67000091+TusharChugh2212@users.noreply.github.com>
2020-08-15 12:46:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+// Function For Animations
+function buttonAnimation(key){
+  document.querySelector("."+key).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+key).classList.remove("pressed");
+  },100);
+}
+// When Button is Clicked
+function clicked() {
+  switch (this.innerHTML) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+      default:console.log(this.innerHTML);
+  }
+  buttonAnimation(this.innerHTML);
+
+}
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", clicked);
+}
+// When key is Pressed
+document.addEventListener("keydown",function(event){
+  switch (event.key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+  }
+  buttonAnimation(event.key);
+})
